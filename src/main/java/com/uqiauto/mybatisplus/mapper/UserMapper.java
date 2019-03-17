bd2d1d4bf3898082a5e305084fc5879f8c102545
huzhiting <18333617859@163.com>
2019-03-17 19:34:41 +0800

    mybatisplus demo

---
+++ src/main/java/com/uqiauto/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.uqiauto.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.uqiauto.mybatisplus.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
